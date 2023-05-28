f6071ce8e158a845b4ef3f70355bdb9e35674736
maiko.trindade <maikocharles@gmail.com>
2023-05-27 21:17:00 -0600

    Added koin

---
+++ app/src/main/java/io/github/maikotrindade/nomadrewards/base/ApplicationController.kt
@@ -0,0 +1,17 @@
+package io.github.maikotrindade.nomadrewards.base
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class ApplicationController : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@ApplicationController)
+        }
+    }
+}
